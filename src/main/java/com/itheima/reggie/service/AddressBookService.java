2756ca1f7b0ca9fb6bd6e1ab619d708700c5b29c
Gentle_Wu <2535504474@qq.com>
2023-06-01 00:14:07 +0800

    Spring Cache缓存更新

--- src/main/java/com/itheima/reggie/service/AddressBookService.java
+++ src/main/java/com/itheima/reggie/service/AddressBookService.java
@@ -3,5 +3,9 @@ package com.itheima.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.reggie.entity.AddressBook;
 
+/**
+ * 地址管理
+ */
+
 public interface AddressBookService extends IService<AddressBook> {
 }
